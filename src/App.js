dbdf15c07bdece7fddbcd478e78f657681e2bf58
onurcankaya <onur.eren.cankaya@gmail.com>
2019-11-24 12:34:39 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import './App.css'
+import { connect } from 'react-redux'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Age-label">
+          Your Age: <span>{this.props.age}</span>
+        </div>
+        <button onClick={this.props.onAgeDown}>Age Down</button>
+        <button onClick={this.props.onAgeUp}>Age Up</button>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    age: state.age,
+  }
+}
+
+const mapDispachToProps = (dispatch) => {
+  return {
+    onAgeUp: () => dispatch({ type: 'AGE_UP', value: 1 }),
+    onAgeDown: () => dispatch({ type: 'AGE_DOWN', value: 1 }),
+  }
+}
+export default connect(mapStateToProps, mapDispachToProps)(App)
